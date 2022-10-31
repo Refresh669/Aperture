e250c5db8be88df9d4f59dbd46e55e356ac106fd
refresh669 <vlad27171@gmail.com>
2022-10-31 07:24:48 +0200

    dot

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $( function() {
     $( "#accordion" ).accordion();
 } );
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:2
+        }
+    }
+});
